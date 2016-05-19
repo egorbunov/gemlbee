d14354bfe1f65427c85b3b822985497833bd334d
Egor Gorbunov <egor-mailbox@ya.ru>
2016-05-19 04:30:15 +0300

    Fix test compile error

--- browser-api/src/test/kotlin/org/jetbrains/bio/browser/desktop/RenderComponentTest.kt
+++ browser-api/src/test/kotlin/org/jetbrains/bio/browser/desktop/RenderComponentTest.kt
@@ -1,6 +1,10 @@
 package org.jetbrains.bio.browser.desktop
 
+import org.jetbrains.bio.browser.LociCompletion
 import org.jetbrains.bio.browser.model.SingleLocationBrowserModel
+import org.jetbrains.bio.browser.query.desktop.DesktopInterpreter
+import org.jetbrains.bio.browser.query.desktop.NewTrackViewListener
+import org.jetbrains.bio.browser.query.desktop.TrackNameListener
 import org.jetbrains.bio.browser.tasks.CancellableState
 import org.jetbrains.bio.browser.tracks.TrackView
 import org.jetbrains.bio.browser.util.Storage
@@ -14,11 +18,13 @@ import kotlin.test.assertFailsWith
 import kotlin.test.assertNotNull
 import kotlin.test.assertNull
 
+
 class RenderComponentTest {
     @Test fun cancellationFailsTheFuture() {
         val flag = AtomicBoolean(false)
         val browser = DesktopGenomeBrowser(
-                GenomeQuery("to1"), listOf(DummyTrackView(flag)))
+                SingleLocationBrowserModel(GenomeQuery("to1")), arrayListOf(DummyTrackView(flag)),
+                LociCompletion.DEFAULT_COMPLETION, DummyInterpreter())
         val c = RenderComponent(browser.trackViews.single(), browser, Storage())
         c.size = Dimension(640, 480)
         c.repaintRequired()
@@ -38,5 +44,25 @@ private class DummyTrackView(private val flag: AtomicBoolean) : TrackView("dummy
             flag.set(true)
             checkCanceled()
         }
+    }
+}
+
+class DummyInterpreter: DesktopInterpreter {
+    override fun isParseable(query: String): Boolean {
+        return false
+    }
+    override fun interpret(query: String): String {
+        return ""
+    }
+    override fun addNewTrackNameListener(listener: TrackNameListener) {
+    }
+    override fun removeNewTrackNameListener(listener: TrackNameListener) {
+    }
+    override fun addNewTrackViewListener(listener: NewTrackViewListener) {
+    }
+    override fun removeNewTrackViewListener(listener: NewTrackViewListener) {
+    }
+    override fun getAvailableNamedTracks(): List<String> {
+        return emptyList()
     }
 }
